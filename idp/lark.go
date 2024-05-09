bf2bb2fe4c65bb4b0409b84650ed9d5408e5ac53
onedot <onedot@users.noreply.github.com>
2024-05-09 08:43:44 +0800

    Update lark.go
    change feishu username

--- idp/lark.go
+++ idp/lark.go
@@ -202,7 +202,7 @@ func (idp *LarkIdProvider) GetUserInfo(token *oauth2.Token) (*UserInfo, error) {
 	userInfo := UserInfo{
 		Id:          larkUserInfo.Data.OpenId,
 		DisplayName: larkUserInfo.Data.EnName,
-		Username:    larkUserInfo.Data.Name,
+		Username:    larkUserInfo.Data.OpenId,
 		Email:       larkUserInfo.Data.Email,
 		AvatarUrl:   larkUserInfo.Data.AvatarUrl,
 	}
