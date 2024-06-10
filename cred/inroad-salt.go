280c0bafcb384690c8515d028f027b89775af54e
ypcai <chinabaozi@yeah.net>
2024-06-10 17:33:21 +0800

    增加inroad

---
+++ cred/inroad-salt.go
@@ -0,0 +1,41 @@
+// Copyright 2022 The Casdoor Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cred
+
+import (
+	"crypto/sha256"
+	"crypto/sha1"
+	"encoding/base64"
+
+	"golang.org/x/crypto/pbkdf2"
+)
+
+type InroadSaltCredManager struct{}
+
+func NewInroadSaltCredManager() *InroadSaltCredManager {
+	cm := &InroadSaltCredManager{}
+	return cm
+}
+
+func (cm *InroadSaltCredManager) GetHashedPassword(password string, userSalt string, organizationSalt string) string {
+	// https://www.keycloak.org/docs/latest/server_admin/index.html#password-database-compromised
+	decodedSalt, _ := base64.StdEncoding.DecodeString(userSalt)
+	res := pbkdf2.Key([]byte(password), decodedSalt, 1000, sha1.size, sha1.New)
+	return base64.StdEncoding.EncodeToString(res)
+}
+
+func (cm *InroadSaltCredManager) IsPasswordCorrect(plainPwd string, hashedPwd string, userSalt string, organizationSalt string) bool {
+	return hashedPwd == cm.GetHashedPassword(plainPwd, userSalt, organizationSalt)
+}
