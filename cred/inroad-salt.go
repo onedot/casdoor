918ccc6dd55b991d399bd64f1d1d7bce2de5c470
ypcai <chinabaozi@yeah.net>
2024-06-10 23:34:23 +0800

    Update inroad-salt.go
    fix:100次改为100次

--- cred/inroad-salt.go
+++ cred/inroad-salt.go
@@ -31,7 +31,7 @@ func NewInroadSaltCredManager() *InroadSaltCredManager {
 func (cm *InroadSaltCredManager) GetHashedPassword(password string, userSalt string, organizationSalt string) string {
 	// https://www.keycloak.org/docs/latest/server_admin/index.html#password-database-compromised
 	decodedSalt, _ := base64.StdEncoding.DecodeString(userSalt)
-	passwordBytes := pbkdf2.Key([]byte(password), decodedSalt, 100, 32, sha1.New)
+	passwordBytes := pbkdf2.Key([]byte(password), decodedSalt, 1000, 32, sha1.New)
 
 	res := append（decodedSalt,passwordBytes）
 	return base64.StdEncoding.EncodeToString(res)
