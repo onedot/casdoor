3ff949511838471e96d1f13173fbe8dd8470f0f9
ypcai <ypcai@in-road.com>
2024-08-09 14:44:27 +0800

    bulid：修改为工智道版本号柜规则

--- web/src/Conf.js
+++ web/src/Conf.js
@@ -30,4 +30,4 @@ export const ThemeDefault = {
   isCompact: false,
 };
 
-export const CustomFooter = "For more infomation, visit https://www.in-road.com (v1.7.1)";
+export const CustomFooter = "For more infomation, visit https://www.in-road.com (v1.7.0.0120240809)";
