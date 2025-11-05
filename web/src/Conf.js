30eedc338190c9dd6a700f69324acdac136fc4cc
ypcai <ypcai@in-road.com>
2025-11-05 19:11:12 +0800

    build：更新版本号 v1.8.0.0020251105

--- web/src/Conf.js
+++ web/src/Conf.js
@@ -30,4 +30,4 @@ export const ThemeDefault = {
   isCompact: false,
 };
 
-export const CustomFooter = "For more infomation, visit https://www.in-road.com (v1.8.0.0020251104)";
+export const CustomFooter = "For more infomation, visit https://www.in-road.com (v1.8.0.0020251105)";
